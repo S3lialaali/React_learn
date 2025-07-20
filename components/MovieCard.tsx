dd530549bc9bcb1c7911c79a7dd2fa49e622b707
sayealaali <s3lialaali090@gmail.com>
2025-07-20 14:12:13 +0300

    minor changes to home page

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,39 +1,39 @@
-import { View, Text, TouchableOpacity, Image, ImageComponent } from 'react-native'
+import { View, Text, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
 import { Link } from 'expo-router'
 import { icons } from '@/constants/icons'
 
-const MovieCard = ({id, poster_path, title, vote_average, release_date}: Movie) => {
+const MovieCard = ({ id, poster_path, title, vote_average, release_date }: Movie) => {
   return (
     <Link href={`/movies/${id}`} asChild>
-        <TouchableOpacity className="w-[30%]">
-           <Image
-           source={{
+      <TouchableOpacity className="w-[30%]">
+        {/* Poster */}
+        <Image
+          source={{
             uri: poster_path
-            ? `https://image.tmdb.org/t/p/w500${poster_path}`
+              ? `https://image.tmdb.org/t/p/w500${poster_path}`
+              : 'https://via.placeholder.com/600x400/1a1a1a/FFFFFF?text=No+Image'
+          }}
+          className="w-full h-44 rounded-t-lg"
+          resizeMode="cover"
+        />
 
-            : 'https:fffff.png//placeholder.co/600x400/1a1a1a/'
-           }}
-           className="w-full h-52 rounded-lg"
-           resizeMode="cover"
-            />
+        {/* Info Box */}
+        <View className="bg-[#101522] p-2 rounded-b-lg">
+          <Text className="text-sm font-semibold text-white mb-1" numberOfLines={1}>
+            {title}
+          </Text>
 
-            <Text className="text-sm font-bold text-white" numberOfLines={1}>{title}</Text>
-            <View className=" flex-row items-center justify-start gap-x-1">
-                <Image source={icons.star} className="size-4" />
-                <Text className="text-xs text-white text-bold uppercase">{(vote_average/2).toFixed(1)}</Text>
-            </View>
+          <View className="flex-row items-center gap-x-1 mb-1">
+            <Image source={icons.star} className="w-4 h-4" />
+            <Text className="text-xs text-white font-bold">{(vote_average / 2).toFixed(1)}</Text>
+          </View>
 
-            <View className=" flex-row items-center justify-between">
-                <Text className="text-xs text-light-300 font-medium mt-1">
-                    {release_date?.split('-')[0]}
-                </Text>
-                {/* <Text className="text-xs font-medium text-light-300 uppercase">Movie</Text> */}
-
-            </View>
-
-        </TouchableOpacity>
-        
+          <Text className="text-xs text-[#CCCCCC] font-medium">
+            {release_date?.split('-')[0]}
+          </Text>
+        </View>
+      </TouchableOpacity>
     </Link>
   )
 }
